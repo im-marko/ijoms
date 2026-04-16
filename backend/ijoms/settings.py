f226915114c84255e3e1828ea1d13f710e83fdfa
magweregwede <iammark263@gmail.com>
2026-04-16 17:19:07 +0200

    Allow Vercel domain in ALLOWED_HOSTS and CORS settings

--- backend/ijoms/settings.py
+++ backend/ijoms/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.vercel.app').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -113,7 +113,7 @@ SIMPLE_JWT = {
 
 # CORS
 CORS_ALLOWED_ORIGINS = os.environ.get(
-    'CORS_ALLOWED_ORIGINS', 'http://localhost:3000'
+    'CORS_ALLOWED_ORIGINS', 'http://localhost:3000,https://ijms-bice.vercel.app'
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
 
